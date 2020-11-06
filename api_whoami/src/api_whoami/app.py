08c0f602d6acad49c8a7dfb996736cf901be10f1
mchlr <mrenn2000@yahoo.de>
2020-11-06 15:26:46 +0100

    Added MessageType and Action System and build first methods

--- api_whoami/src/api_whoami/app.py
+++ api_whoami/src/api_whoami/app.py
@@ -3,10 +3,13 @@ from datetime import datetime, timedelta
 import logging
 import time
 import os
+from typing import List
+from whoami import GameState, WhoAmI
 import jwt
 import json
 import uvicorn
 from uuid import uuid4
+from enum import Enum
 from fastapi import FastAPI, WebSocket
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -26,8 +29,66 @@ LOGGER = logging.getLogger(__name__)
 START_TIME = time.time()
 
 # WhoAmI
-playerlist = {}
+connections = {}
+game = WhoAmI()
 
+class MessageType(Enum):
+    STATE = "game-state"
+    ANSWER = "answer"
+    NAMESUGGESTION = "name-suggestion"
+
+class Actions():
+    # tt = type: MessageType
+    # val = Actual Value that will be passed to the action
+    # pid = PlayerID necessary for some operations
+    async def eval(tt, val, pid):
+        parsed = MessageType(tt)
+
+        print("\n")
+        print("Parsed MessageType: ", parsed)
+
+        if parsed is MessageType.STATE:
+            print("=> ProcessState(val)")
+            await Actions.ProcessState(val)
+
+        if parsed is MessageType.ANSWER:
+            print("=> ProcessAnswer(val)")
+            await Actions.ProcessAnswer(val)
+
+        if parsed is MessageType.NAMESUGGESTION:
+            print("=> ProcessNameSuggestion(val, pid)")
+            await Actions.ProcessNameSuggestion(val, pid)
+
+
+    async def ProcessState(val):
+        if val == "start":
+            await sendNamePoll()
+            
+    async def ProcessAnswer(val):
+        if not val:
+            game.nextPlayer()
+            await sendCurrentPlayer()
+        else:
+            print("Correct answer recorded! :)")
+
+    async def ProcessNameSuggestion(val, pid):
+        print("Now adding Name: ", val, pid)
+        if game.addNameSuggestion(pid, val):
+            # All players have chosen! Let's start!
+            print("asd")
+            seq = game.generateSequence()
+            print("got new name sequence and stuff")
+            print(seq)
+            await sendPlayerSequence(seq)
+
+
+            await sendCurrentPlayer()
+
+        else:
+            print("")
+        
+
+        
 
 def main():
     """Run through uvicorn when run."""
@@ -47,25 +108,15 @@ def index():
 @app.get("/register/{playerName}")
 async def registerPlayer(playerName: str):
     pid = str(uuid4())
-    playerlist[pid] = {"name": playerName, "connection": None}
+    
+    connections[pid] = None
+    game.addPlayer(pid, playerName)
 
     # Update currently connected players
     await sendPlayerlist()
     # Complete registration
-    return {"status": "success", "id": pid, "playerlist": [{"id": pid, "name":playerlist[pid]["name"]} for pid in playerlist]}
+    return {"status": "success", "id": pid, "playerlist": game.getPlayerList()}
 
-# Notify current players about a new player
-async def sendPlayerlist():
-    print("sendPlayerlist()")
-    if playerlist.keys() is not []: 
-        pl = [{"id": pid, "name":playerlist[pid]["name"]} for pid in playerlist]
-        print(pl)
-        for pid in playerlist:
-            if(playerlist[pid]["connection"] is not None):
-                print("Messaging: ", playerlist[pid]["name"])
-                await playerlist[pid]["connection"].send_text(json.dumps({"type":"playerlist", "data":pl}))
-    else:
-        print("No connections yet")
 
 
 
@@ -73,41 +124,70 @@ async def sendPlayerlist():
 
 @app.websocket("/connect/{playerId}")
 async def websocket_endpoint(playerId: str, websocket: WebSocket):
-    print("New Websocket connection from: " + playerlist[playerId]["name"] + " (" + playerId + ")")
-
     await websocket.accept()
-    playerlist[playerId]["connection"] = websocket;
+    # Store connection for later use
+    connections[playerId] = websocket;
 
     while True:
         await onMessage(websocket)
-        
+
+
+# Notify current players about a new player
+async def sendPlayerlist():
+    print("sendPlayerlist()")
+    if game.getPlayerCount() > 0: 
+        for pid in connections:
+            if(connections[pid] is not None):
+                await connections[pid].send_text(json.dumps({"type":"playerlist", "data":game.getPlayerList()}))
+    else:
+        print("No connections yet")
+
+
+async def sendCurrentPlayer():
+    np = game.getCurrentPlayer()
+
+    print("Next player: ", np)
+    for pid in connections:
+            if(connections[pid] is not None):
+                await connections[pid].send_text(json.dumps({"type":"next-player", "data":np}))
+
+
+async def sendNamePoll():
+    print("Polling Names...")
+    for pid in connections:
+        await connections[pid].send_text(json.dumps({"type":"name-poll", "data":True}))
+
+async def sendPlayerSequence(seq):
+    for pid in connections:
+        payload = [{"pid": s["pid"], "target": s["target"] if s["pid"] != pid else "???"} for s in seq]
+        await connections[pid].send_text(json.dumps({"type":"player-sequence", "data":payload}))
+
+
 
 # ********** Game Methods **********
 
 async def onMessage(socket):
 
     pid = ""
-    for xx in playerlist:
-        if(playerlist[xx]["connection"] == socket):
+    for xx in connections:
+        if(connections[xx] == socket):
             pid = xx
 
-    print("Getting data from: " + playerlist[pid]["name"])
-
+    print("Getting Data from id: ", pid)
     # TODO: Create a switch-case that handels the game state according to the received message;
 
-    # data = await socket.receive_json()
-    data = await socket.receive_text()
+    # Throws exception, if the socket is closed; 
+    data = await socket.receive_json()
+    print("Now Testing Actions")
+    await Actions.eval(data["type"], data["value"], pid)
+    print("Eval done^^")
 
-    print("Got Data via WS:")
-    print(data["action"])
-    print(data.action)
 
-@app.get("/start")
+    print("Got data via ws: ", data)
+    MessageType(data["type"])
+    
 async def startGame():
     # Send a push message to all active players    
-    for pid in playerlist: 
-        playerlist[pid]["connection"].send_text("""{"game-state": "start"}""")
-    while True:
-        json = await playerlist[pid]["connection"].receive_json()
-        print("Got new JSON via ws:")
-        print(json)
+    for pid in connections: 
+        connections[pid]["connection"].send_text(json.dumps({"type":"gamestate", "value":"start"}))
+
