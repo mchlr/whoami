616e7484d27b2128a4cdb513d6c3e264b17db4cc
mchlr <mrenn2000@yahoo.de>
2020-11-06 15:28:38 +0100

    added imports

--- app-whoami/src/app/whoami/whoami.module.ts
+++ app-whoami/src/app/whoami/whoami.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { OverviewComponent } from './overview/overview.component';
 import { PlayerCardComponent } from './player-card/player-card.component';
+import { NamePromptComponent } from './name-prompt/name-prompt.component';
 
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 
 
 @NgModule({
   declarations: [
     OverviewComponent,
-    PlayerCardComponent
+    PlayerCardComponent,
+    NamePromptComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
 
     // Imports for Material-UI;
     MatCardModule,
+    MatDialogModule,
+    MatInputModule,
+  ],
+  // Dialog Components
+  entryComponents: [
+    NamePromptComponent
   ]
 })
 export class WhoamiModule { }
