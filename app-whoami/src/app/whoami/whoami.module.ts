0a4efb4daa05597ee46ee3e5bedd4edbe2c36f41
mchlr <mrenn2000@yahoo.de>
2020-11-07 12:58:28 +0100

    Added imports for material buttons

--- app-whoami/src/app/whoami/whoami.module.ts
+++ app-whoami/src/app/whoami/whoami.module.ts
@@ -5,33 +5,38 @@ import { FormsModule } from '@angular/forms';
 import { OverviewComponent } from './overview/overview.component';
 import { PlayerCardComponent } from './player-card/player-card.component';
 import { NamePromptComponent } from './name-prompt/name-prompt.component';
+import { ChallengeDialogComponent } from './player-card/challenge-dialog/challenge-dialog.component';
+import { WinPromptComponent } from './win-prompt/win-prompt.component';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
-
-
-
 
 
 @NgModule({
   declarations: [
     OverviewComponent,
     PlayerCardComponent,
-    NamePromptComponent
+    NamePromptComponent,
+    ChallengeDialogComponent,
+    WinPromptComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
 
     // Imports for Material-UI;
+    MatButtonModule,
     MatCardModule,
     MatDialogModule,
     MatInputModule,
   ],
   // Dialog Components
   entryComponents: [
-    NamePromptComponent
+    NamePromptComponent,
+    ChallengeDialogComponent,
+    WinPromptComponent
   ]
 })
 export class WhoamiModule { }
